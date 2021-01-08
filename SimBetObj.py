baaf59d1e766ac0db6b4495ac184272fa3958312
ltb1999 <huynh365@umn.edu>
2021-01-07 23:37:59 -0600

    Updated to compute upper half triangle matrix for items in a cvs file

--- SimBetObj.py
+++ SimBetObj.py
@@ -8,7 +8,7 @@ import unicodedata
 
 def read_file(filename):
     # Number of items in the list
-    number = 2
+    number = 0
 
     # List of lists of item, each list containing the tag of an item
     list_items = [];
@@ -16,6 +16,7 @@ def read_file(filename):
     # Parse the file to get the tags for each item
     with open(filename,encoding = 'utf-8') as f:
         for line in f:
+            number += 1
             # get the name ID of the item
             try:
                 name_index = line.find("oid")+6
@@ -28,10 +29,49 @@ def read_file(filename):
             except:
                 print("ERROR: Item doesn't have an ID.")
                 return
-    print(list_names)
 
-    # Trial variable for list, should be equivalent to lists_item
-    list = [["ha ha", "hi hi"],["ha ha", "he he"]]
+            # parse for items' tags
+            # combine all tags in one single string
+            total_string = ""
+            try:
+                name_index = line.find("name")+7
+                try:
+                    name = line[name_index:(line.find("produ")-3)]
+                    print(name)
+                    try:
+                        total_string = name.replace(',','')
+                    except:
+                        print("ERROR: Can't split the string in name tag")
+                        return
+                except:
+                    print("ERROR: Can't find \"")
+                    return
+            except:
+                print("ERROR: Item doesn't have a name tag")
+                return
+
+            # Other Tags
+            try:
+                name_index = line.find("tags")+6
+                try:
+                    name = line[name_index:(line.find("ima")-3)]
+                    try:
+                        total_string = name.replace(',','')
+                        total_string = total_string + name
+                    except:
+                        print("ERROR: Can't split the string in other tags")
+                        return
+                except:
+                    print("ERROR: Can't find \"")
+                    return
+            except:
+                print("ERROR: Item doesn't have a tag")
+                return
+            list_items.append(total_string)
+    print(list_items)
+
+    # # Trial variable for list, should be equivalent to lists_item
+    # list = [["ha ha", "hi hi"],["ha ha", "he he"]]
 
     # Design a similarity matrix
     matrix = numpy.zeros(shape=(number,number))
@@ -40,11 +80,11 @@ def read_file(filename):
     # only need to consider the upper half of the matrix because this should be a symmetrix matrix
     for i in range(0,number): #total row is 3
         for j in range(i+1,number): #total column is 3
-            matrix[i][j] = counter_cosine_similarity(Counter(list[i]),Counter(list[j]))
+            matrix[i][j] = counter_cosine_similarity(Counter(list_items[i]),Counter(list_items[j]))
     #print(counter_cosine_similarity(Counter(list[0]),Counter(list[1])))
     print(matrix)
     # Save matrix into a file
-    numpy.savetxt("similarity_items_only.csv", matrix, delimiter=",")
+    numpy.savetxt("similarity_items_only.csv", matrix, fmt = "%.4f", delimiter=",")
 
 def counter_cosine_similarity(c1, c2):
     terms = set(c1).union(c2)
